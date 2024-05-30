9ca1a18a090cb19bdeca2ef476c51a03a2a10362
Khashiev <nxashiev@gmail.com>
2024-05-30 14:21:43 +0300

    add tests

--- src/tests.cc
+++ src/tests.cc
@@ -6,45 +6,120 @@ using namespace s21;
 
 Model model;
 
-TEST(SC, test_1) {
+TEST(CalculatorTest, test_1) {
+  double result = model.Calculate("5 + 5");
+  ASSERT_EQ(result, 10);
+}
+
+TEST(CalculatorTest, test_2) {
+  double result = model.Calculate("7 - 5");
+  ASSERT_EQ(result, 2);
+}
+
+TEST(CalculatorTest, test_3) {
+  double result = model.Calculate("5 * 5");
+  ASSERT_EQ(result, 25);
+}
+
+TEST(CalculatorTest, test_4) {
+  double result = model.Calculate("25 / 5");
+  ASSERT_EQ(result, 5);
+}
+
+TEST(CalculatorTest, test_5) {
+  double result = model.Calculate("5^3");
+  ASSERT_EQ(result, 125);
+}
+
+TEST(CalculatorTest, test_6) {
+  double result = model.Calculate("10 % 3");
+  ASSERT_EQ(result, 1);
+}
+
+TEST(CalculatorTest, test_7) {
+  double result = model.Calculate("2 * (2 + 2)");
+  ASSERT_EQ(result, 8);
+}
+
+TEST(CalculatorTest, test_8) {
+  double result = model.Calculate("sqrt(49)");
+  ASSERT_EQ(result, 7);
+}
+
+TEST(CalculatorTest, test_9) {
+  double result = model.Calculate("sin(0)");
+  ASSERT_EQ(result, 0);
+}
+
+TEST(CalculatorTest, test_10) {
+  double result = model.Calculate("cos(0)");
+  ASSERT_EQ(result, 1);
+}
+
+TEST(CalculatorTest, test_11) {
+  double result = model.Calculate("tan(0)");
+  ASSERT_EQ(result, 0);
+}
+
+TEST(CalculatorTest, test_12) {
+  double result = model.Calculate("ln(1)");
+  ASSERT_EQ(result, 0);
+}
+
+TEST(CalculatorTest, test_13) {
+  double result = model.Calculate("log(100)");
+  ASSERT_EQ(result, 2);
+}
+
+TEST(CalculatorTest, test_14) {
+  double result = model.Calculate("asin(0)");
+  ASSERT_EQ(result, 0);
+}
+
+TEST(CalculatorTest, test_15) {
+  double result = model.Calculate("acos(1)");
+  ASSERT_EQ(result, 0);
+}
+
+TEST(CalculatorTest, test_16) {
+  double result = model.Calculate("atan(0)");
+  ASSERT_EQ(result, 0);
+}
+///
+TEST(CalculatorTest, test_17) {
   double result = model.Calculate(
       "-(1+3*(5%3+100))/50/1*20+-(50^2*(5+3^2)%(3*2)+31*20/"
       "(10*-5^(2+1)))+39^2*48*10*123-50-10");
   ASSERT_EQ(result, 89799655.696);
 }
 
-TEST(SC, test_1_with_spaces) {
+TEST(CalculatorTest, test_18) {
   double result = model.Calculate(
       "-(1+3 *(5%3+ 100))/50 / 1*20+-( 50^2*(5+3^2  "
       ")%(3*2)+31*20/(10*-5^(2+1)) )+39^2*48*10*123-50-10");
   ASSERT_EQ(result, 89799655.696);
 }
 
-TEST(SC, test_2_long_values) {
+TEST(CalculatorTest, test_19) {
   double result = model.Calculate("15120.150+4961254.9271/2.5^2");
   ASSERT_EQ(result, 808920.938336);
 }
 
-TEST(SC, test_3_functions) {
+TEST(CalculatorTest, test_20) {
   double result = model.Calculate(
       "acos(-0.5)+asin(-0.5)+atan(0.1)*cos(30)*sin(20)*tan(45)");
   ASSERT_EQ(result, 1.593531);
 }
 
-TEST(SC, test_4_log_ln_sqrt) {
+TEST(CalculatorTest, test_21) {
   double result =
       model.Calculate("(ln(50)+100)%(2+2.5)+(log(100)*20)^(2+2)+sqrt(25)");
   ASSERT_EQ(result, 2560005.412023);
 }
 
-TEST(SC, test_5_unary_minis) {
+TEST(CalculatorTest, test_22) {
   double result = model.Calculate("-(cos(20)+-10)%2");
   ASSERT_EQ(result, 1.591918);
-}
-
-TEST(SC, test_6_unary_plus) {
-  double result = model.Calculate("5++5++135++(10++10)");
-  ASSERT_EQ(result, 165);
 }
 
 int main(int argc, char **argv) {
